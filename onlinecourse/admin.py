9a847db84ce04e1182d5e7c999ddaacb5fd5ac19
romanes13 <romanosmalamitsis@gmail.com>
2021-06-17 10:51:17 +0000

    Lab2 models and admin modification, superuser v1

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner
+from .models import Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
+class QuestionAdmin(admin.ModelAdmin):
+    model = Question
+    inlines = [ChoiceInline]
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -23,7 +29,7 @@ class LessonAdmin(admin.ModelAdmin):
 
 
 # <HINT> Register Question and Choice models here
-
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
